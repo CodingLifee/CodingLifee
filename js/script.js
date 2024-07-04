0ca19de4b0da1fdfb95ecbae5df2c555308f509b
Luis Henrique <duimbispo@gmail.com>
2024-07-04 13:29:57 -0300

    Criação do header mobile

--- js/script.js
+++ js/script.js
@@ -59,5 +59,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
+// HEADER MOBILE 
+function showMenu(){
+    let menu = document.getElementsByClassName("mobile-links");
+    if(menu[0].style.display == "none"){
+      menu[0].style.display = "block";
+    } else {
+      menu[0].style.display = "none";
+    }
+}
 
